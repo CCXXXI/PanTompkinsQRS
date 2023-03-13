2e60aa0d8941ee90cb4547cb897a2c977f48339d
CCXXXI <ccxxxi@qq.com>
2023-03-14 07:32:26 +0800

    refactor: move output to assets/

--- tests/test_panTompkins.cpp
+++ tests/test_panTompkins.cpp
@@ -1,6 +1,6 @@
 #include "../src/panTompkins.h"
 
 int main() {
-  init("../assets/ecg_data/assets/lead II.txt", "../out.txt");
+  init("../assets/ecg_data/assets/lead II.txt", "../assets/output.txt");
   panTompkins();
 }
