673eef2b24adaf2fa1c9f258bcee14a1d4295600
CCXXXI <ccxxxi@qq.com>
2023-03-14 05:43:43 +0800

    refactor: clean up

--- panTompkins.cpp
+++ panTompkins.cpp
@@ -1,88 +1,5 @@
 /**
- * ------------------------------------------------------------------------------*
- * File: panTompkins.c                                                           *
- *       ANSI-C implementation of Pan-Tompkins real-time QRS detection algorithm *
- * Author: Rafael de Moura Moreira <rafaelmmoreira@gmail.com>                    *
- * License: MIT License                                                          *
- * ------------------------------------------------------------------------------*
- * ---------------------------------- HISTORY ---------------------------------- *
- *    date   |    author    |                     description                    *
- * ----------| -------------| ---------------------------------------------------*
- * 2019/04/11| Rafael M. M. | - Fixed moving-window integral.                    *
- *           |              | - Fixed how to find the correct sample with the    *
- *           |              | last QRS.                                          *
- *           |              | - Replaced constant value in code by its #define.  *
- *           |              | - Added some casting on comparisons to get rid of  *
- *           |              | compiler warnings.                                 *
- * 2019/04/15| Rafael M. M. | - Removed delay added to the output by the filters.*
- *           |              | - Fixed multiple detection of the same peak.       *
- * 2019/04/16| Rafael M. M. | - Added output buffer to correctly output a peak   *
- *           |              | found by back searching using the 2nd thresholds.  *
- * 2019/04/23| Rafael M. M. | - Improved comparison of slopes.                   *
- *           |              | - Fixed formula to obtain the correct sample from  *
- *           |              | the buffer on the back search.                     *
- * ------------------------------------------------------------------------------*
- * MIT License                                                                   *
- *                                                                               *
- * Copyright (c) 2018 Rafael de Moura Moreira                                    *
- *                                                                               *
- * Permission is hereby granted, free of charge, to any person obtaining a copy  *
- * of this software and associated documentation files (the "Software"), to deal *
- * in the Software without restriction, including without limitation the rights  *
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell     *
- * copies of the Software, and to permit persons to whom the Software is         *
- * furnished to do so, subject to the following conditions:                      *
- *                                                                               *
- * The above copyright notice and this permission notice shall be included in all*
- * copies or substantial portions of the Software.                               *
- *                                                                               *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR    *
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,      *
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE   *
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        *
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, *
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE *
- * SOFTWARE.                                                                     *
- *-------------------------------------------------------------------------------*
- * Description                                                                   *
- *                                                                               *
- * The main goal of this implementation is to be easy to port to different opera-*
- * ting systems, as well as different processors and microcontrollers, including *
- * embedded systems. It can work both online or offline, depending on whether all*
- * the samples are available or not - it can be adjusted on the input function.  *
- *                                                                               *
- * The main function, panTompkings(), calls input() to get the next sample and   *
- * store it in a buffer. Then it runs through a chain of filters: DC block, low  *
- * pass @ 15 Hz and high pass @ 5Hz. The filtered signal goes both through a de- *
- * rivative filter, which output is then squared, and through a windowed-integra-*
- * tor.                                                                          *
- *                                                                               *
- * For a signal peak to be recognized as a fiducial point, its correspondent va- *
- * lue on both the filtered signal and the integrator must be above a certain    *
- * threshold. Additionally, there are time-restraints to prevent a T-wave from   *
- * being mistakenly identified as an R-peak: a hard 200ms restrain (a new peak   *
- * 200ms from the previous one is, necessarily, a T-wave) and a soft 360ms res-  *
- * train (the peak's squared slope must also be very high to be considered as a  *
- * real peak).                                                                   *
- *                                                                               *
- * When a peak candidate is discarded, its value is used to update the noise     *
- * thresholds - which are also used to estimate the peak thresholds.             *
- *                                                                               *
- * Two buffers keep 8 RR-intervals to calculate RR-averages: one of them keeps   *
- * the last 8 RR-intervals, while the other keeps only the RR-intervals that res-*
- * pect certain restrictions. If both averages are equal, the heart pace is con- *
- * sidered normal. If the heart rate isn't normal, the thresholds change to make *
- * it easier to detect possible weaker peaks. If no peak is detected for a long  *
- * period of time, the thresholds also change and the last discarded peak candi- *
- * date is reconsidered.                                                         *
- *-------------------------------------------------------------------------------*
- * Instructions                                                                  *
- *                                                                               *
  * Here's what you should change to adjust the code to your needs:               *
- *                                                                               *
- * On panTompkins.h:                                                             *
- * - typedef int dataType;                                                       *
- * Change it from 'int' to whatever format your data is (float, unsigned int etc)*
  *                                                                               *
  * On both panTompkins.h and panTompkins.c:                                      *
  * - void init(char file_in[], char file_out[]);                                 *
@@ -118,41 +35,12 @@
  * If you need them both to have the same amount of samples, set this to 0. If   *
  * you're working with different filters and/or sampling rates, you might need to*
  * adjust this value.                                                            *
- *                                                                               *
- * - #include <stdio.h>                                                          *
- * The file, as it is, both gets its inputs and sends its outputs to files. It   *
- * works on both Windows and Linux. If your source isn't a file, and/or your sys-*
- * tem doesn't have the <stdio.h> header, remove it.                             *
- * Include any other headers you might need to make your implementation work,    *
- * such as hardware libraries provided by your microcontroller manufacturer.     *
- *                                                                               *
- * - The input() function                                                        *
- * Change it to get the next sample from your source (a file, a serial device etc*
- * previously set up in your init() function. Return the sample value or NOSAMPLE*
- * if there are no more available samples.                                       *
- *                                                                               *
- * - The output() function                                                       *
- * Change it to output whatever you see fit, however you see fit: an RR-interval *
- * (which can be sent as a parameter to your function using the RR arrays), the  *
- * index of sample or timestamp which caused a R peak, whether a sample was a R  *
- * peak or not etc, and it can be written on a file, displayed on screen, blink a*
- * LED etc.                                                                      *
- *                                                                               *
- * - The panTompkins() function                                                  *
- * This function is almost entirely ANSI C, which means it should work as is on  *
- * most platforms. The only lines you really have to change are the fclose() ones*
- * at the very end, which are only here to allow testing of the code on systems  *
- * such as Windows and Linux for PC. You may wish to create extra variables or   *
- * increase the buffers' size as you see fit to extract different kinds of infor-*
- * mation to output, or fine tune the detection as you see fit - such as adding  *
- * different conditions for verification, initializing self-updating variables   *
- * with empirically-obtained values or changing the filters.                     *
  *-------------------------------------------------------------------------------*
  */
 
 #define WINDOWSIZE 20   // Integrator window size, in samples. The article recommends 150ms. So, FS*0.15.
 						// However, you should check empirically if the waveform looks ok.
-#define NOSAMPLE -32000 // An indicator that there are no more samples to read. Use an impossible value for a sample.
+#define NOSAMPLE (-32000) // An indicator that there are no more samples to read. Use an impossible value for a sample.
 #define FS 360          // Sampling frequency.
 #define BUFFSIZE 600    // The size of the buffers (in samples). Must fit more than 1.66 times an RR interval, which
                         // typically could be around 1 second.
@@ -162,7 +50,7 @@
 						// in the final end result.
 
 #include "panTompkins.h"
-#include <stdio.h>      // Remove if not using the standard file functions.
+#include <cstdio>      // Remove if not using the standard file functions.
 
 
 FILE *fin, *fout;       // Remove them if not using files and <stdio.h>.
