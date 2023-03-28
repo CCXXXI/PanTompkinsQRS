3c9002cdf231a1d1dc3c48c7ed6aace06cf03b4c
CCXXXI <ccxxxi@qq.com>
2023-03-28 10:55:09 +0800

    test: add test

---
+++ tests/test_panTompkins.cpp
@@ -0,0 +1,23 @@
+#include <catch2/catch_test_macros.hpp>
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+#include "../src/panTompkins.h"
+
+TEST_CASE("test_PanTompkinsQRS", "[PanTompkinsQRS]") {
+  std::ifstream in("../assets/ecg_data/assets/data.json");
+  auto points = nlohmann::json::parse(in);
+
+  std::ifstream out("../assets/output.txt");
+
+  init(125);
+
+  for (auto point : points) {
+    auto actual = panTompkins(point["leadII"]);
+
+    bool expected;
+    out >> std::boolalpha >> expected;
+
+    REQUIRE(actual == expected);
+  }
+}
