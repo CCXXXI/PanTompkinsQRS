98242cb436e61a12c2aec879e9f700a8736fca4f
CCXXXI <ccxxxi@qq.com>
2023-03-28 10:22:28 +0800

    test: read from json

--- tests/test_panTompkins.cpp
+++ tests/test_panTompkins.cpp
@@ -1,14 +1,18 @@
 #include <fstream>
+#include <nlohmann/json.hpp>
 
 #include "../src/panTompkins.h"
 
 int main() {
-  std::ifstream in("../assets/ecg_data/assets/lead II.txt");
+  std::ifstream in("../assets/ecg_data/assets/data.json");
+  nlohmann::json j;
+  in >> j;
+
   std::ofstream out("../assets/output.txt");
 
   init(125);
 
-  for (float sample; in >> sample;) {
-    out << std::boolalpha << panTompkins(sample) << std::endl;
+  for (auto point : j) {
+    out << std::boolalpha << panTompkins(point["leadII"]) << std::endl;
   }
 }
